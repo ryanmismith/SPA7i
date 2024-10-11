14b2e1edfad3d77f1b93641ac7a9778d9024ac58
Ryan Smith <rsmith@sevenislands.com>
2024-10-11 13:53:07 -0400

    Summary and plotting functions

--- R/plotProductVolumes.R
+++ R/plotProductVolumes.R
@@ -1,48 +1,52 @@
-#' Plot Stocking and AAC Volumes
+#' Plot Stocking, AAC, and Annual Species Group Standing Inventory
 #'
-#' @description This function takes the output of the `summarizeProductVolumes` function and creates two plots
-#' to visualize the initial and final stocking volumes and AAC harvests for different species groups. The plots are suitable for publication
-#' and display the data side-by-side for easy comparison.
+#' @description This function takes the output of the `summarizeProductVolumes` function and creates four plots:
+#' 1. A stocking summary showing initial and final standing volumes for each species group.
+#' 2. An AAC (Annual Allowable Cut) summary showing initial and final harvest volumes for each species group.
+#' 3. A line plot showing the total standing inventory by year for each species group.
+#' 4. A line plot showing the AAC by year for each species group.
 #'
-#' The species groups are Cedar, Hardwood, Other Softwood, Pine, Poplar, and Spruce-Fir.
+#' The number of years plotted will be equal to the number of years run in the `run_aac_for_all` function.
 #'
+#' The species groups include Cedar, Hardwood, Other Softwood, Pine, Poplar, and Spruce-Fir.
 #' The function automatically adjusts the y-axis to dynamically scale based on the maximum values in the data.
 #'
-#' @param summary_volumes A dataframe containing summarized product volumes from the `summarizeProductVolumes` function.
+#' @param summary A list containing annual volumes, summary volumes, and hardwood volumes from the `summarizeProductVolumes` function.
 #' @param base_size An integer specifying the base font size for the plot. Defaults to 14.
 #'
-#' @return A list containing two ggplot objects: one for the stocking summary (`stockingplot`) and one for the AAC summary (`aacplot`).
+#' @return A list containing four ggplot objects:
+#' 1. `stockingplot`: Stocking summary of initial and final standing volumes for each species group.
+#' 2. `aacplot`: AAC summary of initial and final harvest volumes for each species group.
+#' 3. `annual_inventory_plot`: Line plot showing annual species group standing inventory.
+#' 4. `annual_aac_plot`: Line plot showing annual species group AAC.
 #' @family Reporting Functions
 #' @seealso \code{\link{summarizeProductVolumes}} for generating the summarized product volumes used as input to this function.
 #'
 #' @examples
 #' \dontrun{
 #' # Example usage:
-#' # Assume `aac_results` and `ratios_df` contain the appropriate data.
-#' summary_volumes <- summarizeProductVolumes(aac_results, ratios_df)
-#'
-#' # Generate the plots
-#' summary_plots <- plotProductVolumes(summary_volumes)
-#'
-#' # Print the generated plots
+#' summary <- summarizeProductVolumes(aac_results, ratios_df)
+#' summary_plots <- plotProductVolumes(summary)
 #' print(summary_plots$stockingplot)
 #' print(summary_plots$aacplot)
+#' print(summary_plots$annual_inventory_plot)
+#' print(summary_plots$annual_inventory_plot)
 #' }
-#' @export
 #'
-plotProductVolumes <- function(summary_volumes, base_size = 14) {
+#' @export
+plotProductVolumes <- function(summary, base_size = 14) {
 
   # Automatically extract the final year from the summary for labeling purposes
-  final_year_label <- max(summary_volumes$final_year, na.rm = TRUE)
+  final_year_label <- max(summary$summary_volumes$final_year, na.rm = TRUE)
 
   # Reshape the data to long format for plotting volumes
-  volume_long <- summary_volumes %>%
+  volume_long <- summary$summary_volumes %>%
     dplyr::select(species_group, initial_volume, final_volume) %>%
     tidyr::gather(key = "year", value = "volume", -species_group) %>%
     dplyr::mutate(year = ifelse(year == "initial_volume", "Year 1", paste0("Year ", final_year_label)))
 
   # Reshape the data to long format for plotting harvests
-  harvest_long <- summary_volumes %>%
+  harvest_long <- summary$summary_volumes %>%
     dplyr::select(species_group, initial_harvest, final_harvest) %>%
     tidyr::gather(key = "year", value = "harvest", -species_group) %>%
     dplyr::mutate(year = ifelse(year == "initial_harvest", "AAC Year 1", paste0("AAC Year ", final_year_label)))
@@ -72,7 +76,7 @@ plotProductVolumes <- function(summary_volumes, base_size = 14) {
       panel.grid.minor = element_blank(),
       plot.background = element_rect(fill = "white"),
       legend.position = "right",
-      legend.title = element_blank(),  # Remove legend title
+      legend.title = element_blank(),
       legend.text = element_text(size = 10)
     )
 
@@ -82,7 +86,7 @@ plotProductVolumes <- function(summary_volumes, base_size = 14) {
     labs(title = "AAC Summary", x = "Species Group", y = "Total Cords") +
     scale_x_discrete() +
     scale_y_continuous(labels = scales::comma, limits = c(0, y_limit_harvest)) +  # Dynamic Y-axis limit
-    scale_fill_manual(values = c("blue", "orange")) +  # Custom colors
+    scale_fill_manual(values = c("blue", "orange")) +
     theme_minimal(base_size = base_size) +
     theme(
       plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
@@ -93,11 +97,66 @@ plotProductVolumes <- function(summary_volumes, base_size = 14) {
       panel.grid.minor = element_blank(),
       plot.background = element_rect(fill = "white"),
       legend.position = "right",
-      legend.title = element_blank(),  # Remove legend title
+      legend.title = element_blank(),
       legend.text = element_text(size = 10)
     )
 
-  # Return the two plots as a list
-  return(list(stockingplot = stocking_plot, aacplot = aac_plot))
+  # Get the maximum values for dynamic scaling
+  max_annual_volume <- max(summary$annual_volumes$total_volume, na.rm = TRUE)
+  max_annual_harvest <- max(summary$annual_volumes$total_harvest, na.rm = TRUE)
+
+  # Add buffer to the y-axis limits (e.g., 10% above the maximum value)
+  y_limit_annual_volume <- max_annual_volume * 1.1
+  y_limit_annual_harvest <- max_annual_harvest * 1.1
+
+  # Create a sequence of years from 1 to the final year
+  year_seq <- seq(1, max(summary$annual_volumes$year), 1)
+
+  ## 3. Annual Species Group Standing Inventory Plot (Line Graph)
+  annual_inventory_plot <- ggplot(summary$annual_volumes, aes(x = year, y = total_volume, color = species_group)) +
+    geom_line(linewidth = 1.1) +
+    labs(title = "Annual Species Group Standing Inventory", x = "Year", y = "Total Cords") +
+    scale_color_manual(values = c("Cedar" = "green", "Hardwood" = "brown", "Other Softwood" = "purple",
+                                  "Pine" = "blue", "Poplar" = "yellow", "Spruce-Fir" = "darkgreen")) +  # Custom colors
+    scale_x_continuous(breaks = year_seq) +  # Sequential X-axis years
+    scale_y_continuous(labels = scales::comma, limits = c(0, y_limit_annual_volume)) +  # Dynamic Y-axis limit
+    theme_minimal(base_size = base_size) +
+    theme(
+      plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+      axis.title = element_text(face = "bold"),
+      axis.text = element_text(color = "black"),
+      panel.grid.major = element_line(color = "#D3D3D3"),
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = "white"),
+      legend.position = "right",
+      legend.title = element_blank(),
+      legend.text = element_text(size = 10)
+    )
+
+  ## 4. Annual Species Group AAC Inventory Plot (Line Graph)
+  annual_aac_plot <- ggplot(summary$annual_volumes, aes(x = year, y = total_harvest, color = species_group)) +
+    geom_line(linewidth = 1.1) +
+    labs(title = "Annual Species AAC", x = "Year", y = "Total Cords") +
+    scale_color_manual(values = c("Cedar" = "green", "Hardwood" = "brown", "Other Softwood" = "purple",
+                                  "Pine" = "blue", "Poplar" = "yellow", "Spruce-Fir" = "darkgreen")) +  # Custom colors
+    scale_x_continuous(breaks = year_seq) +  # Sequential X-axis years
+    scale_y_continuous(labels = scales::comma, limits = c(0, y_limit_annual_harvest)) +  # Dynamic Y-axis limit
+    theme_minimal(base_size = base_size) +
+    theme(
+      plot.title = element_text(face = "bold", size = 16, hjust = 0.5),
+      axis.title = element_text(face = "bold"),
+      axis.text = element_text(color = "black"),
+      panel.grid.major = element_line(color = "#D3D3D3"),
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = "white"),
+      legend.position = "right",
+      legend.title = element_blank(),
+      legend.text = element_text(size = 10)
+    )
+
+  # Return all four plots as a list
+  return(list(stockingplot = stocking_plot, aacplot = aac_plot,
+              annual_inventory_plot = annual_inventory_plot,
+              annual_aac_plot = annual_aac_plot))
 }
 
--- R/summarizeProductVolumes.R
+++ R/summarizeProductVolumes.R
@@ -1,24 +1,37 @@
-#' Summarize product volumes for initial and final years.
+#' Summarize Product Volumes for Initial and Final Years
 #'
 #' @description This function takes the results of the product volume calculations and
 #' aggregates the standing and harvest volumes for specified wood groupings
-#' (hardwood, spruce-fir, cedar, pine, other softwood, poplar) for the initial and final years.
+#' (hardwood, spruce-fir, cedar, pine, other softwood, poplar) for both initial and final years.
+#' It also calculates the percentage of sawlogs and pulp for each year in the hardwood species group.
+#'
+#' The function returns three dataframes:
+#' 1. `annual_volumes`: Summarized standing and harvest volumes for each species group across all years.
+#' 2. `summary_volumes`: Summarized standing and harvest volumes for the first and final years.
+#' 3. `hardwood_volumes`: Detailed breakdown of sawlog and pulp volumes and percentages for hardwood.
 #'
 #' @param aac_results A dataframe containing AAC results for multiple years.
 #' @param ratios_df A dataframe containing the ratios for hardwood and softwood for each township and matrix.
 #' @param years Integer, number of years to calculate product volumes for (default is 20).
 #'
-#' @return A list containing summed volumes for initial and final years, separated by wood groupings.
+#' @return A list containing three dataframes:
+#' 1. `annual_volumes`: Aggregated standing and harvest volumes for each species group across all years.
+#' 2. `summary_volumes`: Summarized standing and harvest volumes for the first and final years.
+#' 3. `hardwood_volumes`: Detailed breakdown of sawlog and pulp volumes and percentages for the hardwood species group.
+#'
 #' @family Reporting Functions
 #' @seealso \code{\link{calculateProductRatios}} and \code{\link{run_aac_for_all}} to create `aac_results` and `ratios_df`.
+#'
 #' @examples
 #' \dontrun{
 #' # Example usage:
 #' summary_volumes <- summarizeProductVolumes(aac_results, ratios_df)
-#' print(summary_volumes)
+#' print(summary_volumes$annual_volumes)
+#' print(summary_volumes$summary_volumes)
+#' print(summary_volumes$hardwood_volumes)
 #' }
 #' @export
-#'
+
 summarizeProductVolumes <- function(aac_results, ratios_df, years = 20) {
 
   # Call calculateProductVolumes to get the product volumes dataframe
@@ -42,26 +55,73 @@ summarizeProductVolumes <- function(aac_results, ratios_df, years = 20) {
     "WPl" = "Pine", "WPp" = "Pine"
   )
 
+
   # Add wood type mapping and groupings to product_volumes
-  product_volumes <- product_volumes %>%
+  product_volumes <- product_volumes |>
     dplyr::mutate(
       wood_type = wood_type_mapping[product],
-      species_group = groupings[product]
+      species_group = groupings[product],
+      product_class = dplyr::case_when(
+        endsWith(product, "l") | endsWith(product, "t") ~ "sawlog",
+        endsWith(product, "p") ~ "pulp",
+        TRUE ~ "other"
+      )
     )
+
+  # Get annual standing and harvest volumes
+  annual_volumes <- product_volumes |>
+    dplyr::group_by(year, species_group) |>
+    dplyr::summarize(total_volume = sum(product_standing_volume, na.rm = TRUE),
+                     total_harvest = sum(product_harvest_volume, na.rm = TRUE)) |>
+    dplyr::filter(total_volume > 0) |> dplyr::ungroup()
+
+  # Hardwood Sawlogs to Pulp
+  hardwood_volumes <- product_volumes |>
+    filter(species_group == "Hardwood") |>
+    dplyr::group_by(year, species_group, product_class) |>
+    dplyr::summarize(
+      total_volume = sum(product_standing_volume, na.rm = TRUE),
+      total_harvest = sum(product_harvest_volume, na.rm = TRUE)
+    ) |>
+    dplyr::ungroup() |>
+    # Calculate total volumes for sawlog and pulp within each year
+    dplyr::group_by(year, species_group) |>
+    dplyr::mutate(
+      total_sawlog_volume = sum(total_volume[product_class == "sawlog"], na.rm = TRUE),
+      total_pulp_volume = sum(total_volume[product_class == "pulp"], na.rm = TRUE),
+      total_sawlog_harvest = sum(total_harvest[product_class == "sawlog"], na.rm = TRUE),
+      total_pulp_harvest = sum(total_harvest[product_class == "pulp"], na.rm = TRUE)
+    ) |>
+    # Calculate product_percent and harvest_percent
+    dplyr::mutate(
+      product_percent = dplyr::case_when(
+        product_class == "sawlog" ~ total_volume / (total_sawlog_volume + total_pulp_volume),
+        product_class == "pulp" ~ total_volume / (total_sawlog_volume + total_pulp_volume),
+        TRUE ~ NA_real_
+      ),
+      harvest_percent = dplyr::case_when(
+        product_class == "sawlog" ~ total_harvest / (total_sawlog_harvest + total_pulp_harvest),
+        product_class == "pulp" ~ total_harvest / (total_sawlog_harvest + total_pulp_harvest),
+        TRUE ~ NA_real_
+      )
+    ) |>
+    dplyr::ungroup() |>
+    mutate(group = "hardwood") |>
+    select(group, year, product_class, total_volume, total_harvest, product_percent, harvest_percent)
 
   # Get the first year (1) and final year (where sum(product_standing_volume) > 0)
   first_year <- 1
-  final_year <- product_volumes %>%
-    dplyr::group_by(year) %>%
-    dplyr::summarize(total_volume = sum(product_standing_volume, na.rm = TRUE)) %>%
-    dplyr::filter(total_volume > 0) %>%
-    dplyr::pull(year) %>%
+  final_year <- product_volumes |>
+    dplyr::group_by(year) |>
+    dplyr::summarize(total_volume = sum(product_standing_volume, na.rm = TRUE)) |>
+    dplyr::filter(total_volume > 0) |>
+    dplyr::pull(year) |>
     max()
 
   # Summarize for the initial year
-  initial_summary <- product_volumes %>%
-    dplyr::filter(year == first_year) %>%
-    dplyr::group_by(species_group) %>%
+  initial_summary <- product_volumes |>
+    dplyr::filter(year == first_year) |>
+    dplyr::group_by(species_group) |>
     dplyr::summarize(
       start_year = max(year),
       initial_volume = round(sum(product_standing_volume, na.rm = TRUE), 0),
@@ -69,9 +129,9 @@ summarizeProductVolumes <- function(aac_results, ratios_df, years = 20) {
     )
 
   # Summarize for the final year
-  final_summary <- product_volumes %>%
-    dplyr::filter(year == final_year) %>%
-    dplyr::group_by(species_group) %>%
+  final_summary <- product_volumes |>
+    dplyr::filter(year == final_year) |>
+    dplyr::group_by(species_group) |>
     dplyr::summarize(
       final_year = max(year),
       final_volume = round(sum(product_standing_volume, na.rm = TRUE), 0),
@@ -81,5 +141,6 @@ summarizeProductVolumes <- function(aac_results, ratios_df, years = 20) {
   # Merge the initial and final summaries
   summary_volumes <- dplyr::full_join(initial_summary, final_summary, by = "species_group")
 
-  return(summary_volumes)
+  return(list(annual_volumes = annual_volumes, summary_volumes = summary_volumes, hardwood_volumes = hardwood_volumes))
+
 }
